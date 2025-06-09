e42f7b79d94f17de79b71c6aed4a395d57ec21b4
Illia Harkusha <illia.harkusha.w@gmail.com>
2025-06-09 19:48:26 +0300

    Add note model bundling

--- esbuild.config.js
+++ esbuild.config.js
@@ -1,4 +1,5 @@
 const esbuild = require('esbuild');
+const copy = require("esbuild-plugin-copy");
 
 esbuild.build({
     entryPoints: ['src/init.js'],
@@ -6,4 +7,14 @@ esbuild.build({
     outfile: 'dist/_ug-im-bundle.js',
     minify: true,
     platform: 'browser',
+    plugins: [
+        copy.copy({
+            assets: [
+                {
+                    from: ['./src/Country - Map [Experimental].html'],
+                    to: ['./']
+                }
+            ]
+        })
+    ]
 }).catch(() => process.exit(1));
